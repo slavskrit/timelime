1fcfaed843cce4d99084c5323250b189911408c6
slavskrit <dpronin@yandex.ru>
2024-05-07 17:57:39 -0700

    single day works

--- src/Bar.tsx
+++ src/Bar.tsx
@@ -12,8 +12,12 @@ function getOffset(day: number): string {
   return (day / daysInYear * 100).toFixed(2);
 }
 
-function getEntryStyle(color: string): string {
+function getBarStyle(color: string = "orange"): string {
   return `h-10 bg-${color}-300 flex items-center opacity-75 justify-center rounded-md my-1`;
+}
+
+function getArrowStyle(color: string = "orange"): string {
+  return `-rotate-45 h-9 w-9 bg-${color}-300 flex items-center opacity-75 justify-center rounded-md my-1`;
 }
 
 const daysInYear = 365; // TODO:Handle viscosny).
@@ -24,33 +28,24 @@ interface BarEntryProps {
 export function Bar(props: BarEntryProps) {
   const entry: BarEntry = props.entry;
   const shape = entry.endDay - entry.startDay < 10 ? 'arrow' : 'bar';
-  return <Switch fallback={<div>Bar is not supported</div>}>
-    <Match when={shape === "arrow"}>
-      <div class='has-tooltip'>
-        <div style={{
-          "transform": `translateX(${getOffset(entry.startDay)}cqw)`,
-          "width": "5rem",
-        }} class={getEntryStyle(entry.entry.color ?? "orange")}>
-          <span class='tooltip rounded shadow-xl p-2 bg-gray-100'>
-            {entry.entry.start} → {entry.entry.end}
-          </span>
-          <p class="text-sm">{entry.entry.name}</p>
+  return <div class='has-tooltip' style={{
+    "transform": `translateX(${getOffset(entry.startDay)}cqw)`,
+  }}><span class='tooltip rounded shadow-xl p-2 bg-gray-100'>
+      {entry.entry.start} → {entry.entry.end}
+    </span>
+    <Switch fallback={<div>Bar is not supported</div>}>
+      <Match when={shape === "arrow"}>
+        <div class={getArrowStyle(entry.entry.color)}>
+          <p class="rotate-45 text-sm stroke-cyan-500">{entry.entry.name}</p>
         </div>
-      </div>
-    </Match>
-    <Match when={shape === "bar"}>
-      <div class='has-tooltip'>
+      </Match>
+      <Match when={shape === "bar"}>
         <div style={{
-          "transform": `translateX(${getOffset(entry.startDay)}cqw)`,
           "width": `${getOffset(entry.endDay - entry.startDay)}%`
-        }} class={getEntryStyle(entry.entry.color ?? "orange")}>
-          <span class='tooltip rounded shadow-xl p-2 bg-gray-100'>
-            {entry.entry.start} → {entry.entry.end}
-          </span>
+        }} class={getBarStyle(entry.entry.color)}>
           <p class="text-sm">{entry.entry.name}</p>
         </div>
-      </div >
-    </Match>
-  </Switch >
-
+      </Match>
+    </Switch >
+  </div >
 }
